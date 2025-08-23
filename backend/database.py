1637337ac45e75fa68b9e2b62f0508bd57b56e3a
Bruce Peters <brucebpeters12@gmail.com>
2025-08-23 16:54:10 -0700

    Changes to allow for docker

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,14 @@
+import dotenv
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
-URL_DATABASE = "sqlite:///./phoneme_db.db"
+dotenv.load_dotenv()
+URL_DATABASE = os.getenv("DATABASE_URL", "")
 
-engine = create_engine(URL_DATABASE, connect_args={"check_same_thread": False})
+engine = create_engine(URL_DATABASE)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 metadata = MetaData()

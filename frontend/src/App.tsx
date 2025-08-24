0a65b5e0ecef3d7d4b91ad90a2617483e3471b6c
Bruce Peters <brucebpeters12@gmail.com>
2025-08-24 15:42:21 -0700

    Memory limits and non-automatic restart - also made frontend support the backend for next vercel deploy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route /*Outlet*/ } from "react-router-dom";
-// import Dashboard from "./pages/Dashboard.tsx";
-// import Layout from "./components/Layout.tsx";
-// import Login from "./pages/Login.tsx";
-// import SignUp from "./pages/SignUp.tsx";
-// import ProgressDashboard from "./pages/ProgressDashboard.tsx";
-// import Settings from "./pages/Settings.tsx";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.tsx";
+import Layout from "./components/Layout.tsx";
+import Login from "./pages/Login.tsx";
+import SignUp from "./pages/SignUp.tsx";
+import ProgressDashboard from "./pages/ProgressDashboard.tsx";
+import Settings from "./pages/Settings.tsx";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
-// import OAuthRedirect from "./components/OAuthRedirect.tsx";
+import OAuthRedirect from "./components/OAuthRedirect.tsx";
 import { ThemeProvider } from "./contexts/ThemeContext.tsx";
 import { SettingsProvider } from "./contexts/SettingsContext.tsx";
-// import PracticeRouter from "./pages/PracticeRouter.tsx";
-// import PracticeDashboard from "./pages/PracticeDashboard.tsx";
+import PracticeRouter from "./pages/PracticeRouter.tsx";
+import PracticeDashboard from "./pages/PracticeDashboard.tsx";
 import LandingPage from "./pages/LandingPage.tsx";
 import UnderConstructionPage from "./pages/UnderConstructionPage.tsx";
 import { Analytics } from "@vercel/analytics/react";
@@ -25,7 +25,7 @@ function App() {
             <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
               <Routes>
                 <Route path="/" element={<LandingPage />} />
-                {/*  <Route path="/login" element={<Login />} />
+                <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/oauth-callback" element={<OAuthRedirect />} />
                 <Route
@@ -43,7 +43,7 @@ function App() {
                   <Route path="/progress" element={<ProgressDashboard />} />
                   <Route path="/settings" element={<Settings />} />
                   <Route path="/practice" element={<PracticeDashboard />} />
-                </Route>*/}
+                </Route>
                 <Route path="*" element={<UnderConstructionPage />} />
               </Routes>
             </ThemeProvider>

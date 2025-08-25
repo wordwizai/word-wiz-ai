247ba6ddd299eb0247b14bbb12c2bd9693dc1ceb
Bruce Peters <brucebpeters12@gmail.com>
2025-08-24 18:33:35 -0700

    testing change to see if it works on vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,4 +11,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: "/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 });

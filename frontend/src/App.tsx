f753fffe48e47ed017947ed4f1f0bcf9a1707263
Bruce Peters <brucebpeters12@gmail.com>
2025-08-28 22:27:38 -0700

    fixing strange undo git thing idk

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,8 +13,10 @@ import PracticeRouter from "./pages/PracticeRouter.tsx";
 import PracticeDashboard from "./pages/PracticeDashboard.tsx";
 import LandingPage from "./pages/LandingPage.tsx";
 import UnderConstructionPage from "./pages/UnderConstructionPage.tsx";
+import ProtectedRoute from "./components/ProtectedRoute.tsx";
 import { Analytics } from "@vercel/analytics/react";
 import About from "./pages/About.tsx";
+import Contact from "./pages/Contact.tsx";
 
 function App() {
   return (
@@ -28,17 +30,24 @@ function App() {
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/oauth-callback" element={<OAuthRedirect />} />
                 <Route
                   path="/practice/:sessionId"
-                  element={<PracticeRouter />}
+                  element={
+                    <ProtectedRoute>
+                      <PracticeRouter />
+                    </ProtectedRoute>
+                  }
                 />
                 <Route
                   element={
-                    <Layout>
-                      <Outlet />
-                    </Layout>
+                    <ProtectedRoute>
+                      <Layout>
+                        <Outlet />
+                      </Layout>
+                    </ProtectedRoute>
                   }
                 >
                   <Route path="/dashboard" element={<Dashboard />} />
